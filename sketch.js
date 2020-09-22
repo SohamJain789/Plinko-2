ee99afdeffb44d261fd647fb21a01f1d05632097
SohamJain789 <69146549+SohamJain789@users.noreply.github.com>
2020-09-22 14:41:35 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,153 +1,125 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+var Engine = Matter.Engine,
+    World = Matter.World,
+    Events = Matter.Events,
+    Bodies = Matter.Bodies; 
+var particles = [];
+var plinkos = [];
+var divisions =[];
+var particle;
 
-var engine, world;
-var stand1,stand2;
-var block1,block2,block3,block4,block5,block6
-var block7,block8,block9,block10,block11,block12;
-var block13,block14,block15,block16;
-var block17,block18,block19,block20,block21;
-var block22,block23,block24,block25,sling,polygon;
-var score = 0;
+var divisionHeight=300;
+var score =0;
+var count = 0;
+var gameState ="start";
+
 function setup() {
-  createCanvas(1500,500);
+  createCanvas(800, 800);
   engine = Engine.create();
   world = engine.world;
-  //createSprite(400, 200, 50, 50);
-  stand1 = new Ground(400,300,300,20)
-  stand2 = new Ground(1160,380,240,20)
-  //you are trying to create the ground before creating the physics engine & the world
-  //engine = Engine.create();
-  //world = engine.world;
+  ground = new Ground(width/2,height,width,20);
 
-  //First
-  block1 = new Box(310,290,40,40);
-  block2 = new Box(340,290,40,40);
-  block3 = new Box(370,290,40,40);
-  block4 = new Box(400,290,40,40);
-  block5 = new Box(430,290,40,40);
-  block6 = new Box(460,290,40,40);
-  block7 = new Box(490,290,40,40);
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+    for (var j = 75; j <=width; j=j+50) {
+       plinkos.push(new Plinko(j,75));
+    }
 
-  block8 = new Box(340,250,40,40);
-  block9 = new Box(370,250,40,40);
-  block10 = new Box(400,250,40,40);
-  block11 = new Box(430,250,40,40);
-  block12 = new Box(460,250,40,40);
+    for (var j = 50; j <=width-10; j=j+50) {
+        plinkos.push(new Plinko(j,175));
+    }
+
+    for (var j = 75; j <=width; j=j+50) {
+        plinkos.push(new Plinko(j,275));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) {
+        plinkos.push(new Plinko(j,375));
+    }
+    
+}
  
-  block13 = new Box(370,220,40,40);
-  block14 = new Box(400,220,40,40);
-  block15 = new Box(430,220,40,40);
-
-  block16 = new Box(400,180,40,40)
-  
-  //Second
-  block17 = new Box(1100,370,40,40);
-  block18 = new Box(1130,370,40,40);
-  block19 = new Box(1160,370,40,40);
-  block20 = new Box(1190,370,40,40);
-  block21 = new Box(1220,370,40,40);
-
-  block22 = new Box(1130,340,40,40);
-  block23 = new Box(1160,340,40,40);
-  block24 = new Box(1190,340,40,40);
-
-  block25 = new Box(1160,300,40,40);
-
-  polygon = new Polygon(100,200,90,70)
-  sling = new SlingShot(polygon.body,{x:100,y:200});
-}
-
 function draw() {
-  background(247,247,247);
+  background("black");
+  textSize(35)
+  text("Score : "+score,20,40);
+  fill("white");
+  //text(mouseX + "," + mouseY, 20, 50);
+  textSize(35)
+  text(" 500 ", 5, 550);
+  text(" 500 ", 80, 550);
+  text(" 500 ", 160, 550);
+  text(" 500 ", 240, 550);
+  text(" 100 ", 320, 550);
+  text(" 100 ", 400, 550);
+  text(" 100 ", 480, 550);
+  text(" 200 ", 560, 550);
+  text(" 200 ", 640, 550);
+  text(" 200 ", 720, 550);
+  Engine.update(engine);
+  ground.display();
+  
+  if ( gameState =="end") {
+    
+    textSize(100);
+    text("GameOver", 150, 250);
+    //return
+  }
 
-    noStroke();
-    textSize(35);
-    fill(0,0,0);
-    text("Score:"+ score,width-350, 50);
+  
 
-     Engine.update(engine);
-     stand1.display(); 
-     stand2.display();
+  
 
-     block1.display();
-     block2.display();
-     block3.display();
-     block4.display();
-     block5.display();
-     block6.display();
-     block7.display();
-
-     block8.display();
-     block9.display();
-     block10.display();
-     block11.display();
-     block12.display();
-
-     block13.display();
-     block14.display();
-     block15.display();
-
-     block16.display();
-
-     block17.display();
-     block18.display();
-     block19.display();
-     block20.display();
-     block21.display();
-
-     block22.display();
-     block23.display();
-     block24.display();
-
-     block25.display();
-
-     block1.score();
-     block2.score();
-     block3.score();
-     block4.score();
-     block5.score();
-     block6.score();
-     block7.score();
-     block8.score();
-     block9.score();
-     block10.score();
-     block11.score();
-     block12.score();
-     block13.score();
-     block14.score();
-     block15.score();
-     block16.score();
-     block17.score();
-     block18.score();
-     block19.score();
-     block21.score();
-     block22.score();
-     block23.score();
-     block24.score();
-     block25.score();
+  for (var i = 0; i < plinkos.length; i++) {
+     plinkos[i].display();  
+  }
+ 
+    if(particle!=null)
+    {
+       particle.display();
+        
+        if (particle.body.position.y>760)
+        {
+              if (particle.body.position.x < 300) 
+              {
+                  score=score+500;      
+                  particle=null;
+                  if ( count>= 5) gameState ="end";                          
+              }
 
 
-     
-     polygon.display();
-     
-     //block2.display(); 
-    //drawSprites();
+              else if (particle.body.position.x < 600 && particle.body.position.x > 301 ) 
+              {
+                    score = score + 100;
+                    particle=null;
+                    if ( count>= 5) gameState ="end";
+
+              }
+              else if (particle.body.position.x < 900 && particle.body.position.x > 601 )
+              {
+                    score = score + 200;
+                    particle=null;
+                    if ( count>= 5)  gameState ="end";
+
+              }      
+              
+        }
+  
+      }
+
+   for (var k = 0; k < divisions.length; k++) 
+   {
+     divisions[k].display();
+   }
+ 
 }
 
-function mouseDragged(){
-Matter.Body.setPosition(polygon.body,{x:mouseX , y:mouseY})
-}
 
-function mouseReleased(){
-sling.fly();
-}
-
-function keyPressed(){
- if(keyCode === 32){
- sling.attach(polygon.body);
- }
-
+function mousePressed()
+{
+  if(gameState!=="end")
+  {
+      count++;
+     particle=new Particle(mouseX, 10, 10, 10); 
+  }   
 }
